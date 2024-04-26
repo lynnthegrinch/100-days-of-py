78c69d258d26ef69421ca34f2b0902ed7f113976
ValentinaLynn <143955817+lynnthegrinch@users.noreply.github.com>
2024-04-26 18:21:15 -0400

    Create weeks.py

---
+++ weeks.py
@@ -0,0 +1,6 @@
+age = input("what is ur current age? ")
+age_in_weeks = int(age) * 52
+weeks_in_life = 90 * 52
+weeks_left = int(weeks_in_life) - int(age_in_weeks)
+
+print(f"you have {weeks_left} weeks left in your life")
