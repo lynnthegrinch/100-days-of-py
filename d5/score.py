cbf1250c712a9e217d2324b746ab00f7d8367d49
ValentinaLynn <143955817+lynnthegrinch@users.noreply.github.com>
2024-04-29 19:25:44 -0400

    Create score.py

---
+++ d5/score.py
@@ -0,0 +1,10 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+h_s = 0
+for score in student_scores:
+  if score > h_s
+  h_s = score
+print(f"The highest score in the class is: {h_s}")
