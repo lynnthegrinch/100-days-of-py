30e3d372a74d8219926ae118c449af6bcd65de45
ValentinaLynn <143955817+lynnthegrinch@users.noreply.github.com>
2024-04-29 20:26:21 -0400

    Create passgen.py

---
+++ d5/passgen.py
@@ -0,0 +1,20 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+print("welcome to the password genrator")
+nr_letters = int(input("how many letters would you like in your password?\n"))
+nr_symbols = int(input("how many symbols would you like?\n"))
+nr_numbers = int(input("how many numbers would you like?\n"))
+password = ""
+for letter in range(1,nr_letters +1):
+    password = random.choice(letters) + password
+for symbol in range(1, nr_symbols +1):
+    password = random.choice(symbols) + password
+for number in range(1, nr_numbers +1):
+    password = random.choice(numbers) + password
+
+
+
+print(password)
+print(pass_shuff)
