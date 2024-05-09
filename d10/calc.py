4e23417a337abc43bd98b5aedd52059591bb57aa
ValentinaLynn <143955817+lynnthegrinch@users.noreply.github.com>
2024-05-09 18:27:59 -0400

    Update calc.py

--- d10/calc.py
+++ d10/calc.py
@@ -9,13 +9,13 @@ def div(n1, n2):
 
 operations = {"+": add, "-": sub, "*": mult, "/": div,}
 def calc():
-    n1= int(input("gimme number"))
+    n1= float(int(input("gimme number")))
     for i in operations:
         print(i)
     keep_going = True
     while keep_going:
         choice = input("what operation do you want to do?")
-        n2 = int(input("gimme number"))
+        n2 = float(int(input("gimme number")))
         calc_func = operations[choice]
         answer = calc_func(n1, n2)
         print(f"{n1} {choice} {n2} = {answer}")
